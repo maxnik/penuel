b2358c3b6dba837badcfc3d3aa24ae3ea4445809
Max Nikolenko <maxim.nikolenko@gmail.com>
2014-06-14 00:02:25 +0300

    Let admins edit user info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
+
   def new
     @user = User.new
   end
 
   def create
-    @user = User.new(create_user_params)
+    @user = User.new(user_params)
     if @user.save
       redirect_to users_url
     else
@@ -23,6 +25,11 @@ class UsersController < ApplicationController
   end
 
   def update
+    if @user.update_attributes(user_params)
+      redirect_to users_url
+    else
+      render action: 'edit'
+    end
   end
 
   def destroy
@@ -30,7 +37,11 @@ class UsersController < ApplicationController
 
   private 
 
-  def create_user_params 
+  def user_params 
     params.require(:user).permit(:email, :name_ru, :name_en, :locale, :role)
   end 
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
